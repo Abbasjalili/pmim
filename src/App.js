e8246f8624d6b9ca15cab95f1bc7955997f902cc
Abbas Jalili <79339337+Abbasjalili@users.noreply.github.com>
2023-11-11 18:34:38 -0800

    barchart updated

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import BarChart from './components/BarChart';
 import { UserData } from './Data';
+import { useState } from 'react';
 
 function App() {
-  const [ UserData, setUserData] = useState({
+  const [userData, setUserData]= useState({
     labels: UserData.map((data) => data.year),
     datasets: [{
       label: "Users Gained",
-      data: UserData.map((data) => data.userGain)
-    }]
-  })
+      data: UserData.map((data) => data.userGain),
+      backgroundColor: [
+        "rgba(75,192,192,1", 
+        "#ecf0f1",
+        "#50AF95",
+        "#f3ba2f",
+        "#2a71d0",
+      ],
+    },
+  ],
+})
+
+
   return (
     <div className="App">
-      <BarChart chartData={UserData}/>
+      <div style = {{ width: 700}}>
+        <BarChart chartData={userData} />
+      </div>
     </div>
   );
 }
